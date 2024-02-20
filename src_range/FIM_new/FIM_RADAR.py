update/fix the range measurement variance / FIM

src_range/FIM_new/FIM_RADAR.py
@@ -19,7 +19,7 @@ from src_range.control.Sensor_Dynamics import state_multiple_update
 #
 
 @jit
-def Multiple_JU_FIM_Radar(radar_states,target_state,Js,A,Q,Pt,Gt,Gr,L,lam,rcs,B,alpha,c):
+def Multiple_JU_FIM_Radar(radar_states,target_state,Js,A,Q,Pt,Gt,Gr,L,lam,rcs,fc,sigmaW,c):
     target_state = target_state.reshape(1,-1)
     N,dn= radar_states.shape
     M,dm = target_state.shape
@@ -40,11 +40,11 @@ def Multiple_JU_FIM_Radar(radar_states,target_state,Js,A,Q,Pt,Gt,Gr,L,lam,rcs,B,
 
     K = Pt * Gt * Gr * lam**2 * rcs / (4*np.pi)**3 / L
 
-    C = c**2 / (alpha*B**2) * 1/K
+    C = c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
 
     # dd^T / ||d||^4 * rho * rho/(rho+1)
     # jnp.einsum("ijk,ilm->ikm", d, d)
-    coef = jnp.sqrt((1/(C*distances**6) + 8/(distances**4)))
+    coef = jnp.sqrt((4/(C*distances**6) + 8/(distances**4)))
     outer_vector = d * coef
     outer_product = (outer_vector.transpose(0,2,1) @ outer_vector)
 
@@ -65,7 +65,7 @@ def Multiple_JU_FIM_Radar(radar_states,target_state,Js,A,Q,Pt,Gt,Gr,L,lam,rcs,B,
 
 
 @jit
-def Single_JU_FIM_Radar(radar_states,target_states,J,A,Q,Pt,Gt,Gr,L,lam,rcs,B,alpha,c):
+def Single_JU_FIM_Radar(radar_states,target_states,J,A,Q,Pt,Gt,Gr,L,lam,rcs,fc,c,sigmaV,sigmaW):
 
     N,dn= radar_states.shape
     M,dm = target_states.shape
@@ -73,12 +73,12 @@ def Single_JU_FIM_Radar(radar_states,target_states,J,A,Q,Pt,Gt,Gr,L,lam,rcs,B,al
     radar_states = jnp.concatenate((radar_states,jnp.zeros((N,1))),-1)
     target_positions = target_states[:,:dm//2]
 
-    # Qinv = jnp.linalg.inv(Q+jnp.eye(dm)*1e-8)
+    Qinv = jnp.linalg.inv(Q+jnp.eye(dm*M)*1e-8)
     # # Qinv = jnp.linalg.inv(Q)
     #
-    # D11 = A.T @ Qinv @ A
-    # D12 = -A.T @ Qinv
-    # D21 = D12.T
+    D11 = A.T @ Qinv @ A
+    D12 = -A.T @ Qinv
+    D21 = D12.T
 
     d = (target_positions[jnp.newaxis,:,:] - radar_states[:,jnp.newaxis,:])
 
@@ -86,48 +86,42 @@ def Single_JU_FIM_Radar(radar_states,target_states,J,A,Q,Pt,Gt,Gr,L,lam,rcs,B,al
 
     K = Pt * Gt * Gr * lam**2 * rcs / (4*np.pi)**3 / L
 
-    C = c**2 / (alpha*B**2) * 1/K
+    C = c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
 
     # dd^T / ||d||^4 * rho * rho/(rho+1)
     # jnp.einsum("ijk,ilm->ikm", d, d)
-    coef = jnp.sqrt((1/(C*distances**6) + 8/(distances**4)))
+    coef = jnp.sqrt((4/(C*distances**6) + 8/(distances**4)))
     outer_vector = d * coef
     outer_product = (outer_vector.transpose(1,2,0) @ outer_vector.transpose(1,0,2))
+    J_standard = jax.scipy.linalg.block_diag(*[jax.scipy.linalg.block_diag(outer_product[m],jnp.eye(dm//2)* 1 / (sigmaV**2)) for m in range(M)])
 
-    # append zeros because there is no velocity in the radar equation...
+    D22 = J_standard + Qinv
 
-    # zeros = jnp.zeros_like(d)
-    # d = jnp.concatenate((d,zeros),-1)
-
-    # dd^T / ||d||^4 * rho * rho/(rho+1)
-    # jnp.einsum("ijk,ilm->ikm", d, d)
-
-    # D22 = jnp.sum(outer_product,axis=0) + Qinv
-
-    # J = D22 - D21 @ jnp.linalg.inv(J + D11) @ D12
-    J = jax.scipy.linalg.block_diag(*[outer_product[m] for m in range(M)])
+    J = D22 - D21 @ jnp.linalg.inv(J + D11) @ D12
+    # J = jax.scipy.linalg.block_diag(*[outer_product[m] for m in range(M)])
 
     return J
 
-@jit
-def Single_FIM_Radar(radar_states,target_states,Pt,Gt,Gr,L,lam,rcs,c,B,alpha):
+# @jit
+def Single_FIM_Radar(radar_states,target_states,Pt,Gt,Gr,L,lam,rcs,fc,c,sigmaW):
     N,dn= radar_states.shape
     M,dm = target_states.shape
 
-    d = (target_states[jnp.newaxis,:,:] - radar_states[:,jnp.newaxis,:])
+    target_positions = target_states[:,:dm//2]
+
+    d = (target_positions[jnp.newaxis,:,:] - radar_states[:,jnp.newaxis,:])
 
     distances = jnp.sqrt(jnp.sum(d**2,-1,keepdims=True))
 
     K = Pt * Gt * Gr * lam**2 * rcs / (4*np.pi)**3 / L
 
-    C = c**2 / (alpha*B**2) * 1/K
+    # C =  sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
+    C =  c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
+    coef = jnp.sqrt((4/(C*distances**6) + 8/(distances**4)))
 
-    # dd^T / ||d||^4 * rho * rho/(rho+1)
-    # jnp.einsum("ijk,ilm->ikm", d, d)
-    coef = jnp.sqrt((1/(C*distances**6) + 8/(distances**4)))
     outer_vector = d * coef
-    outer_product = (outer_vector.transpose(1,2,0) @ outer_vector.transpose(1,0,2))
 
+    outer_product = (outer_vector.transpose(1,2,0) @ outer_vector.transpose(1,0,2))
 
     J = jax.scipy.linalg.block_diag(*[outer_product[m] for m in range(M)])
     return J
@@ -166,7 +160,7 @@ def Multi_FIM_Logdet_decorator_MPC(IM_fn,method="action"):
 
             return -multi_FIM_obj
 
-    elif method=="Single_FIM_2D_action":
+    elif method=="Single_FIM_3D_action":
         @jit
         def Multi_FIM_Logdet(U,chis,radar_states,target_states,time_step_sizes,J,
                              A,
@@ -189,7 +183,7 @@ def Multi_FIM_Logdet_decorator_MPC(IM_fn,method="action"):
                 multi_FIM_obj += gamma**(t-1) * logdet
 
 
-                target_states = (A @ target_states.reshape(-1, dm).T).T.reshape(M, dm)
+                target_states = (A @ target_states.reshape(-1, M*dm).T).T.reshape(M, dm)
 
             return -multi_FIM_obj
 
@@ -202,7 +196,7 @@ def Multi_FIM_Logdet_decorator_MPC(IM_fn,method="action"):
             N, dn = radar_states.shape
             # ps = jnp.expand_dims(ps,1)
 
-            sign, logdet = jnp.lnalg.slogdet(IM_fn(radar_states=radar_states,target_states=target_states))
+            sign, logdet = jnp.linalg.slogdet(IM_fn(radar_states=radar_states,target_states=target_states))
 
             return -logdet
 
@@ -211,7 +205,7 @@ def Multi_FIM_Logdet_decorator_MPC(IM_fn,method="action"):
 
 @partial(jit,static_argnames=['N',"space"])
 def FIM_2D_Visualization(ps,qs,
-                      Pt,Gt,Gr,L,lam,rcs,c,B,alpha,N,space):
+                      Pt,Gt,Gr,L,lam,rcs,fc,c,sigmaW,N,space):
 
 
     sensor_and_targets = jnp.vstack((ps,qs))
@@ -230,11 +224,11 @@ def FIM_2D_Visualization(ps,qs,
 
     K = Pt * Gt * Gr * lam**2 * rcs / (4*np.pi)**3 / L
 
-    C = c**2 / (alpha*B**2) * 1/K
+    C = c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
 
     # dd^T / ||d||^4 * rho * rho/(rho+1)
     # jnp.einsum("ijk,ilm->ikm", d, d)
-    coef = jnp.sqrt((1/(C*distances**6) + 8/(distances**4)))
+    coef = jnp.sqrt((4/(C*distances**6) + 8/(distances**4)))
 
     outer_vector = d * coef
 
@@ -256,13 +250,13 @@ def FIM_2D_Visualization(ps,qs,
 
 @partial(jit,static_argnames=['N'])
 def FIM_Visualization(ps,qs,
-                      Pt,Gt,Gr,L,lam,rcs,s,N):
+                      Pt,Gt,Gr,L,lam,rcs,fc,c,sigmaW,N):
 
 
     sensor_and_targets = jnp.vstack((ps,qs[:,:2]))
 
-    x_max,y_max = jnp.min(sensor_and_targets,axis=0)-30
-    x_min,y_min = jnp.max(sensor_and_targets,axis=0)+30
+    x_max,y_max = jnp.min(sensor_and_targets,axis=0)-300
+    x_min,y_min = jnp.max(sensor_and_targets,axis=0)+300
 
     qx,qy = jnp.meshgrid(jnp.linspace(x_min,x_max,N),
                          jnp.linspace(y_min,y_max,N))
@@ -275,9 +269,12 @@ def FIM_Visualization(ps,qs,
 
     K = Pt * Gt * Gr * lam**2 * rcs / (4*np.pi)**3 / L
 
-    constant = jnp.sqrt(4 / (distances**8 * s**2 ) * K**2 / (K + s**2 * distances**4 ))
+    C = c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
 
-    outer_vector = jnp.expand_dims(d * constant,-1)
+
+    coef = jnp.sqrt((4/(C*distances**6) + 8/(distances**4)))
+
+    outer_vector = jnp.expand_dims(d * coef,-1)
 
     # dd^T / ||d||^4 * rho * rho/(rho+1)
     outer_product =  outer_vector @ outer_vector.transpose(0,1,3,2)

src_range/FIM_new/JU_Radar_Constraints_test.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     os.makedirs(photo_dump, exist_ok=True)
 
 
-    Restarts = 25
+    Restarts = 10
     N = 6
 
     # ==================== RADAR CONFIGURATION ======================== #
@@ -64,26 +64,27 @@ if __name__ == "__main__":
     L = 1;
     alpha = (jnp.pi)**2 / 3
     B = 0.05 * 10**5
+    Pt = 10000
+    K = Pt * Gt * Gr * lam ** 2 * rcs / L / (4 * jnp.pi) ** 3
 
 
 
     # calculate Pt such that I achieve SNR=x at distance R=y
     R = 100
-
-    K = Gt * Gr * lam ** 2 * rcs / L / (4 * jnp.pi) ** 3
-    coef = K / (R ** 4)
+    Pr = K / (R ** 4)
 
 
-    SCNR = -20
-    CNR = -10
-    Pt = 10000
-    Amp, Ma, zeta, s = NoiseParams(Pt * coef, SCNR, CNR=CNR)
+    SNR = 0
+    # calculate Pt such that I achieve SNR=x at distance R=y
+    sigmaW = jnp.sqrt(Pr / (10**(SNR/10)))
 
-    print("Spread: ",s**2)
-    print("Power Return (RCS): ",coef*Pt)
+    # C = c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
+
+    # print("Power Return (RCS): ",coef*Pt)
     print("K",K)
 
     print("Pt (peak power)={:.9f}".format(Pt))
+    print("Noise Power={:.5f}".format(sigmaW**2))
     print("lam ={:.9f}".format(lam))
 
     # ==================== SENSOR CONSTRAINTS ======================== #
@@ -95,22 +96,22 @@ if __name__ == "__main__":
     #
     ps = jax.random.uniform(key, shape=(N, 2), minval=-2000, maxval=2000)
     qs = jnp.array([
-                    [-2500,-2500.], #,
-                    [2500,2500], #,
-                    [2000,-2500]])
+                    [-2500,-2500.,25,25], #,
+                    [2500,2500,-20,6], #,
+                    [2000,-2500,-10,-10]])
 
     M, dm = qs.shape;
     N ,dn = ps.shape;
 
 
-    IM_fn = partial(Single_FIM_Radar,Pt=Pt,Gt=Gt,Gr=Gr,L=L,lam=lam,rcs=rcs,c=c,B=B,alpha=alpha)
+    IM_fn = partial(Single_FIM_Radar,Pt=Pt,Gt=Gt,Gr=Gr,L=L,lam=lam,rcs=rcs,fc=fc,c=c,sigmaW=sigmaW)
 
     Multi_FIM_Logdet = Multi_FIM_Logdet_decorator_MPC(IM_fn=IM_fn,method=method)
 
     constraints = []
     def distance_constraint_sensors_to_targets(ps_optim):
         ps_optim = ps_optim.reshape(N,dn)
-        difference = (qs[jnp.newaxis, :,:] - ps_optim[:, jnp.newaxis, :])
+        difference = (qs[jnp.newaxis, :,:dm//2] - ps_optim[:, jnp.newaxis, :])
         distance = jnp.sqrt(jnp.sum(difference ** 2, -1))
 
         return distance.ravel()
@@ -143,15 +144,15 @@ if __name__ == "__main__":
         print("Obj Init",objective(ps))
 
 
-        SLSQP = minimize(fun=objective, x0=ps.ravel(),method="SLSQP",constraints=constraints,options={"maxiter":10000,"disp":True},jac=jac,hess=hess)
-        print(SLSQP)
-        ps_optim = SLSQP.x.reshape(N,dn)
+        solver = minimize(fun=objective, x0=ps.ravel(),method="SLSQP",constraints=constraints,options={"maxiter":10000,"disp":True},jac=jac,hess=hess)
+        print(solver)
+        ps_optim = solver.x.reshape(N,dn)
         # ps = ps_optim
 
         J = IM_fn(radar_states=ps_optim, target_states=qs)
         print("Matrix Rank: ",jnp.linalg.matrix_rank(J))
-        if f_best > SLSQP.fun:
-            f_best = SLSQP.fun
+        if f_best > solver.fun:
+            f_best = solver.fun
             ps_best = ps_optim
 
         print("\n")
@@ -199,11 +200,11 @@ if __name__ == "__main__":
     axes[0].plot([],[],"m--",label="Target Boundary")
     axes[0].plot(ps_best[:,0],ps_best[:,1],"ro",label="Radar")
     axes[0].plot(ps[:,0],ps[:,1],"rX",label="Radar Init")
-    axes[0].set_title(f"Best Log |J| = -{np.round(best_obj,5)}")
+    axes[0].set_title(f"Best Log |J| = {-np.round(best_obj,5)}")
     axes[0].legend()
 
-    qx, qy, logdet_grid = FIM_2D_Visualization(ps=ps_best, qs=qs,
-                                            Pt=Pt, Gt=Gt, Gr=Gr, L=L, lam=lam, rcs=rcs,c=c, B=B,alpha=alpha,
+    qx, qy, logdet_grid = FIM_2D_Visualization(ps=ps_best, qs=qs[:,:dm//2],
+                                            Pt=Pt, Gt=Gt, Gr=Gr, L=L, lam=lam, rcs=rcs,c=c,fc=fc, sigmaW=sigmaW,
                                             N=2500,space=1000)
 
     CS = axes[1].contourf(qx, qy, logdet_grid, levels=30)

src_range/FIM_new/JU_Radar_test.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     update_steps = 0
     FIM_choice = "radareqn"
     measurement_choice = "radareqn"
-    method = "Single_FIM_2D_action"
+    method = "Single_FIM_3D_action"
 
     # Save frames as a GIF
     gif_filename = "radar_optimal_RICE.gif"
@@ -65,25 +65,28 @@ if __name__ == "__main__":
     B = 0.05 * 10**5
 
     # calculate Pt such that I achieve SNR=x at distance R=y
-    R = 1000
+    R = 100
 
-    K = Gt * Gr * lam ** 2 * rcs / L / (4 * jnp.pi) ** 3
-    coef = K / (R ** 4)
-
-
-    SCNR = -20
-    CNR = -10
     Pt = 10000
-    Amp, Ma, zeta, s = NoiseParams(Pt * coef, SCNR, CNR=CNR)
+    K = Pt * Gt * Gr * lam ** 2 * rcs / L / (4 * jnp.pi) ** 3
+    Pr = K / (R ** 4)
 
-    print("Spread: ",s**2)
-    print("Power Return (RCS): ",coef*Pt)
+    # get the power of the noise of the signal
+    SNR=0
+    sigmaW = jnp.sqrt(Pr/ (10**(SNR/10)))
+
+
+
+    C = c**2 * sigmaW**2 / (jnp.pi**2 * 8 * fc**2) * 1/K
+
+
+
+    print("Power Return (RCS): ",Pr)
+    print("Noise Power: ",sigmaW**2)
     print("K",K)
 
     print("Pt (peak power)={:.9f}".format(Pt))
     print("lam ={:.9f}".format(lam))
-
-    key_args = {"Pt": Pt, "Gt": Gt, "Gr": Gr, "lam": lam, "L": L, "rcs": rcs, "R": 100,"SCNR":SCNR,"CNR":CNR,"s":s}
 
     # ==================== SENSOR DYNAMICS CONFIGURATION ======================== #
     time_steps = 15
@@ -125,7 +128,8 @@ if __name__ == "__main__":
     # assert (jnp.sum(paretos) <= (1 + 1e-5)) and (jnp.sum(paretos) >= -1e-5), "Pareto weights don't sum to 1!"
 
 
-    sigmaQ = np.sqrt(10 ** 2);
+    sigmaQ = jnp.sqrt(10 ** 2);
+    sigmaV = jnp.sqrt(9)
 
     print("SigmaQ (state noise)={}".format(sigmaQ))
 
@@ -166,7 +170,7 @@ if __name__ == "__main__":
     # Js = jnp.stack([jnp.eye(d) for m in range(M)])
     J = jnp.eye(dm*M) #jnp.stack([jnp.eye(d) for m in range(M)])
 
-    IM_fn = partial(Single_JU_FIM_Radar,A=A_single,Q=Q_single,Pt=Pt,Gt=Gt,Gr=Gr,L=L,lam=lam,rcs=rcs,c=c,B=B,alpha=alpha)
+    IM_fn = partial(Single_JU_FIM_Radar,A=A,Q=Q,Pt=Pt,Gt=Gt,Gr=Gr,L=L,lam=lam,rcs=rcs,fc=fc,c=c,sigmaV=sigmaV,sigmaW=sigmaW)
     # IM_fn(ps,qs[[0],:],Js=Js)
     IM_fn(ps,qs,J=J)
 
@@ -209,7 +213,7 @@ if __name__ == "__main__":
         U = lbfgsb.run(U, bounds=bounds, chis=chis, radar_states=ps, target_states=m0,
                        time_step_sizes=time_step_sizes,
                        J=J,
-                       A=A_single,
+                       A=A,
                        gamma=gamma,
                        ).params
 
@@ -250,8 +254,8 @@ if __name__ == "__main__":
             axes[0].set_title(f"k={k}")
 
             qx,qy,logdet_grid = FIM_Visualization(ps=ps, qs=m0,
-                                                  Pt=Pt,Gt=Gt,Gr=Gr,L=L,lam=lam,rcs=rcs,s=s,
-                                                  N=250)
+                                                  Pt=Pt,Gt=Gt,Gr=Gr,L=L,lam=lam,rcs=rcs,fc=fc,c=c,sigmaW=sigmaW,
+                                                  N=1000)
 
             axes[1].contourf(qx, qy, logdet_grid, levels=20)
             axes[1].scatter(ps[:, 0], ps[:, 1], s=50, marker="x", color="r")

src_range/tracking/Particle_Filter.py
@@ -10,6 +10,7 @@ from jax import vmap
 import functools
 import jax
 from jax import jit
+from jax.tree_util import Partial as partial
 
 import numpy as np
 
@@ -28,7 +29,7 @@ def RangeVelocityMeasure(qs, ps):
     qs = qs[:,:dm//2]
 
     differences = (ps[:, jnp.newaxis] - qs[jnp.newaxis, :])
-    ranges = jnp.sqrt(jnp.sum((differences ** 2), -1,keepdims=True))  # + 1e-8
+    ranges = 2*jnp.sqrt(jnp.sum((differences ** 2), -1,keepdims=True))  # + 1e-8
 
 
     measure = jnp.concatenate((ranges,vs),axis=-1)
@@ -38,7 +39,7 @@ def RangeVelocityMeasure(qs, ps):
     return measure
 
 
-def generate_samples(key, XT, PT, A, Q,
+def generate_range_samples(key, XT, PT, A, Q,
                      Gt,Gr,Pt,lam,rcs,L,c,B,alpha,sigmaV,
                      TN):
     _, subkey = jax.random.split(key)
@@ -53,6 +54,15 @@ def generate_samples(key, XT, PT, A, Q,
 
     key, subkey = random.split(key)
     QK = jax.random.multivariate_normal(subkey, mean=jnp.zeros(Q.shape[0], ), cov=Q, shape=(TN,), method="svd")
+
+    key, subkey = random.split(key)
+    # noise for the velocities
+    RV = jax.random.multivariate_normal(subkey,mean=jnp.zeros(nx//2*M*N,),cov=jnp.eye(nx//2 * M*N)*sigmaV**2,shape=(TN,))
+
+    # coef of noise for RANGE measurements
+    K = Pt * Gt * Gr * lam**2 * rcs / (4*np.pi)**3 / L
+    C = c**2 / (alpha*B**2) * 1/K
+
     key, subkey = random.split(key)
 
     for k in tqdm(range(TN)):
@@ -63,12 +73,19 @@ def generate_samples(key, XT, PT, A, Q,
         # measure onward!
         YT = RangeVelocityMeasure(XT_noise.reshape(M, nx), PT).ravel()
 
-
+        # noise for the ranges
+        key, subkey = random.split(key)
+        range_measures = YT.reshape(M*N,(nx//2 + 1))[:,:1].ravel()
+        rr = jax.random.multivariate_normal(subkey, mean=range_measures, cov=jnp.eye(M*N)*C*jnp.diag(range_measures**4), shape=())
+        rv = RV[k,:].reshape(M*N,nx//2)
+        rr = rr.reshape(M*N,1)
+        YT_noise = YT.reshape(M*N,(nx//2+1)) + jnp.concatenate((rr,rv),axis=-1)
+        YT_noise = YT_noise.ravel()
 
         # append state and measurement
         XT_trajectories.append(XT_noise.reshape(-1, ))
         # YT_trajectories.append(YT_noise.reshape(N, )) # if I sum the radar powers
-        YT_trajectories.append(YT_noise.reshape(N * M, ))
+        YT_trajectories.append(YT_noise.reshape(N * M, nx//2 + 1))
 
     return key, jnp.stack(XT_trajectories, axis=0), jnp.stack(YT_trajectories, axis=0)
 
@@ -87,17 +104,28 @@ def optimal_importance_dist_sample(key, Xprev, A, Q):
     return key, Xnext
 
 
-def weight_update(Wprev, Vnext, s,ynext):
-    # Rayleigh
-    # Wnext =  Wprev * ss.expon.pdf(ynext.ravel(),scale=Vnext.squeeze()+PN,loc=0).prod(axis=1,keepdims=True)
 
-    # Rice
-    # rice_power(Pr=ynext.T,K=K, Pr_avg=Vnext.squeeze()).prod(axis=1,keepdims=True) #jnp.expand_dims(Wnext(Vnext),1)
+def weight_update(Wprev, Vnext,ynext,Pt,Gt,Gr,lam,rcs,L,c,B,alpha,sigmaV,M,N,dm,dn):
 
-    # RICE + AWGN
-    # Amp, Ma, zeta, s = NoiseParams(Vnext.squeeze(), SCNR, CNR=CNR)
-    Wnext = Wprev * ss.ncx2.pdf(ynext.ravel(), df=2, nc=Vnext.squeeze() / (s ** 2), scale=s ** 2).prod(axis=1,
-                                                                                                       keepdims=True)
+    ynext = ynext.reshape(M*N,dm//2 + 1)
+    velocity_measures = Vnext.reshape(-1, M * N, (dm // 2 + 1))[:, :, 1:]
+    range_measures = Vnext.reshape(-1,M * N, (dm // 2 + 1))[:, :, :1]
+
+    velocity_pdf = vmap(lambda velocities: jax.scipy.stats.multivariate_normal.pdf(ynext[:,1:].ravel(),mean=velocities.ravel(),cov=sigmaV**2),in_axes=(0,))
+
+    velocity_pdf = velocity_pdf(velocity_measures) #jax.scipy.stats.multivariate_normal.pdf(ynext[:,1:],mean=velocity_measures,
+                                        #cov=jnp.eye((dm // 2)) * sigmaV**2).prod(axis=-1)
+
+    # coef of noise for RANGE measurements
+    K = Pt * Gt * Gr * lam ** 2 * rcs / (4 * np.pi) ** 3 / L
+    C = c ** 2 / (alpha * B ** 2) * 1 / K
+
+    range_pdf = vmap(lambda ranges: jax.scipy.stats.multivariate_normal.pdf(ynext[:,:1].ravel(),mean=ranges.ravel(),cov=jnp.diag(ranges.ravel())**4*C),in_axes=(0,))
+
+    range_pdf = range_pdf(range_measures)
+
+
+    Wnext = jnp.reshape(velocity_pdf*range_pdf,(-1,1)) * Wprev
 
     Wnext = Wnext / np.sum(Wnext)
 

src_range/tracking/Particle_Filter_test.py
@@ -22,9 +22,9 @@ def create_frame(t, Xnext, XT, ps, m0, ws, neffs, vs, heights, axes=None):
     wmin, wmax = jnp.min(ws), jnp.max(ws)
     ws_minmax = (ws - wmin) / (wmax - wmin + 1e-16)
     pf_point = (Wnext * Xnext).sum(axis=0).ravel()
-    pf_point = pf_point.reshape(M, d)
-    Xnext = Xnext.reshape(-1, M, d)
-    XT = XT.reshape(-1, M, d)
+    pf_point = pf_point.reshape(M, dm)
+    Xnext = Xnext.reshape(-1, M, dm)
+    XT = XT.reshape(-1, M, dm)
 
     pfs = axes[0].scatter(Xnext[:, :, 0], Xnext[:, :, 1], c='green', alpha=ws_minmax, label="Particle States")
     target_traj = axes[0].plot(XT[:, :, 0], XT[:, :, 1], marker='d', color="r", alpha=0.3, label='_nolegend_')
@@ -46,7 +46,7 @@ def create_frame(t, Xnext, XT, ps, m0, ws, neffs, vs, heights, axes=None):
     axes[2].set_ylabel("Neff")
     axes[2].set_xlabel("t")
 
-    neffs = axes[3].plot(vs, 'o-')
+    ranges = axes[3].plot(vs, 'o-')
     axes[3].set_ylabel("Radar Measure")
     axes[3].set_xlabel("t")
 
@@ -127,10 +127,10 @@ if __name__ == "__main__":
     # [-5.4,3.32,z_elevation,-5,-5,0]])
 
     M, dm = qs.shape;
-    N = len(ps);
+    N , dn = ps.shape
 
     sigmaQ = np.sqrt(10 ** (3));
-    sigmaV = np.sqrt(10** (-1))
+    sigmaV = np.sqrt(5)
 
     print("SigmaQ (state noise)={}".format(sigmaQ))
 
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     nx = Q.shape[0]
 
     # Generate the trajectory and measurements to trajectory: X_1:k, Y_1:k
-    key, XT, YT = generate_samples(key, qs, ps, A=Q, Q=Q,
+    key, XT, YT = generate_range_samples(key, qs, ps, A=A, Q=Q,
                                    Gt=Gt,Gr=Gr,Pt=Pt,lam=lam,rcs=rcs,L=L,c=c,B=B,alpha=alpha,sigmaV=sigmaV,
                                    TN=TN)
 
@@ -177,7 +177,9 @@ if __name__ == "__main__":
     m0 = qs.at[:, dm//2:].add(5);
     m0 = m0.at[:, :dm//2].add(-20)
 
-    P0_singular = jnp.diag(jnp.array([50, 50, 50, 50, 50, 50]));
+    # P0_singular = jnp.diag(jnp.array([50, 50, 50, 50, 50, 50]));
+    P0_singular = jnp.diag(jnp.array([50, 50, 50, 50]));
+
     P0 = jnp.kron(jnp.eye(M), P0_singular)
 
     Xprev = jax.random.multivariate_normal(subkey, m0.ravel(), P0, shape=(NP,), method="cholesky")
@@ -195,23 +197,24 @@ if __name__ == "__main__":
 
     frame_names = []
     fig, axes = plt.subplots(nrows=1, ncols=5, figsize=(25, 5));
-    Vs = jnp.zeros((TN, N * M))
+    Vs = jnp.zeros((TN, N * M * (dm//2 + 1)))
     heights = jnp.zeros((TN, M))
-    Vnext_parallel = vmap(functools.partial(RadarEqnMeasure,Gt=Gt,Gr=Gr,Pt=Pt,lam=lam,rcs=rcs,L=L), in_axes=(0, None))
+    Vnext_parallel = vmap(RangeVelocityMeasure, in_axes=(0, None))
+    weight_update_partial = partial(weight_update,Pt=Pt,Gt=Gt,Gr=Gr,lam=lam,rcs=rcs,L=L,c=c,B=B,alpha=alpha,sigmaV=sigmaV,M=M,N=N,dm=dm,dn=dn)
     for t in tqdm(range(0, TN)):
         # get the measurement  y_k+1
         ynext = YT[t].reshape(-1, 1)
 
         # sample with state transition matrix x_k+1
         key, Xnext = optimal_importance_dist_sample(key, Xprev, A, Q)
-        heights = heights.at[t].set(Xnext.reshape(-1, M, d).mean(axis=0)[:, 2])
+        heights = heights.at[t].set(Xnext.reshape(-1, M, dm).mean(axis=0)[:, 2])
 
         # Generate the expected Measurements for x_k+1 (v_k+1)
         Vnext = Vnext_parallel(Xnext.reshape(-1, M, nx // M), ps)
         Vs = Vs.at[t].set(Vnext.mean(0).ravel())
 
         # Update particle weights recursively...
-        Wnext = weight_update(Wprev, Vnext, s,ynext)
+        Wnext = weight_update_partial(Wprev, Vnext,ynext)
         neffs = effective_samples(Wnext)
         Measures.append(Vnext)
         Neffs.append(neffs)
@@ -223,7 +226,7 @@ if __name__ == "__main__":
             break
 
         if t % every_other_frame == 0:
-            frame = create_frame(t, Xnext, XT[:t + 1], ps, m0, Wnext, Neffs, YT[:t + 1], heights[:t + 1], axes=axes)
+            frame = create_frame(t, Xnext, XT[:t + 1], ps, m0, Wnext, Neffs, YT[:t + 1,:,0], heights[:t + 1], axes=axes)
             frame_names.append(frame)
 
         if neffs < (NP * 2 / 3) or (t + 1) % 250 == 0: